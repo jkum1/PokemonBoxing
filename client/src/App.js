14285b2f5aef7a8959e7bbae7884039b0401b8de
Jinho Kum <Jinho.kum@gmail.com>
2021-12-03 11:00:20 -0800

    working 3d model with threeJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Suspense} from 'react';
 import './App.css';
+import {Canvas} from '@react-three/fiber';
+import Controls from './Controls.js';
+import Bulbasaur from './Bulbasaur.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Canvas
+        colorManagement
+        shadows
+        camera={{position: [-5, 10, 10], fov: 60}}
+      >
+        <Controls/>
+        <ambientLight intensity={0.3}/>
+        <pointLight position={[-10,0,-20]} intensity={0.5}/>
+        <pointLight position={[0,-10,0]} intensity={1.5}/>
+        <directionalLight
+          castShadow
+          position={[0,10,0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
+        <group>
+          <mesh receiveShadow rotation={[-Math.PI/2,0,0]} position={[0,-3,0]}>
+            <planeBufferGeometry attach='geometry' args={[100,100]}/>
+            <shadowMaterial attach='material' opacity={0.3}/>
+            <meshPhysicalMaterial attach="material" color="#eba4f7" />
+          </mesh>
+        </group>
+
+        <Suspense fallback={null}>
+          <Bulbasaur/>
+        </Suspense>
+
+      </Canvas>
+    </>
   );
 }
 
